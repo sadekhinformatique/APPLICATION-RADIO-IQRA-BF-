5ff2f3298bc8b1a6e03e1405c6063924806f6c6b
sadekhinformatique <155910956+sadekhinformatique@users.noreply.github.com>
2026-01-05 12:12:40 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -74,7 +74,7 @@ const App: React.FC = () => {
         {/* Footer */}
         <footer className="bg-gray-900 border-t border-gray-800 py-6">
           <div className="max-w-7xl mx-auto px-4 text-center text-gray-500 text-sm">
-            <p>&copy; {new Date().getFullYear()} VoxNova Radio. Tous droits réservés.</p>
+            <p>&copy; {new Date().getFullYear()} radio iqra . Tous droits réservés.</p>
           </div>
         </footer>
       </div>
